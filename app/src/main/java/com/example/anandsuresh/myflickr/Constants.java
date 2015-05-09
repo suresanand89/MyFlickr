dae56c3235ff915baf5dffbc1af9a842d20698d7
suresanand89 <suresanand@gmail.com>
2015-05-08 21:45:00 -0700

    First Commit

---
+++ app/src/main/java/com/example/anandsuresh/myflickr/Constants.java
@@ -0,0 +1,20 @@
+package com.example.anandsuresh.myflickr;
+
+/**
+ * Created by anandsuresh on 5/8/15.
+ */
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "1f53fbbc54bd731c996a4c128a6e4594";
+    public static final String SECRET = "184d5325bbd857df";
+    //    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+    public static final String RADIUS = "2";
+
+    public static final String LATITUDE = "34.063607";
+    public static final String LONGITUDE = "-118.170823";
+
+
+
+}
